4588340665fa615f9abe16ee43466023024bc202
Mark <mark.aaron.fisher@gmail.com>
2017-03-17 09:38:57 -0700

    add properties of event class with getter and setter methods and tests thereof

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -3,7 +3,45 @@ import java.util.List;
 import java.util.Arrays;
 
 public class Event{
+
+  private String mFood;
+  private int mNumGuests;
+  private String mBeverage;
+  private String mEntertainment;
+
   public Event(){
-    
+
+
   }
+
+  public String getFood(){
+    String returnVal = "test";
+    return returnVal;
+  }
+  public void setFood(String usrFood){
+
+  }
+  public Integer getNumGuests(){
+    Integer returnVal = 3;
+    return returnVal;
+  }
+  public void setNumGuests(int usrNumGuests){
+
+  }
+  public String getBeverage(){
+    String returnVal = "test";
+    return returnVal;
+  }
+  public void setBeverage(String usrBeverage){
+
+  }
+  public String getEntertainment(){
+    String returnVal = "test";
+    return returnVal;
+  }
+  public void setEntertainment(String entertainment){
+
+  }
+
+
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -7,4 +7,29 @@ public class EventTest {
     Event testEvent = new Event();
     assertEquals(true, testEvent instanceof Event);
   }
+
+  @Test
+  public void getFood_instantiatesCorrectly_true(){
+    Event testEvent = new Event();
+    assertEquals(true, testEvent.getFood() instanceof String);
+  }
+
+  @Test
+  public void getNumGuests_instantiatesCorrectly_true(){
+    Event testEvent = new Event();
+    assertEquals(true, testEvent.getNumGuests() instanceof Integer);
+  }
+
+  @Test
+  public void getBeverage_instantiatesCorrectly_true(){
+    Event testEvent = new Event();
+    assertEquals(true, testEvent.getBeverage() instanceof String);
+  }
+
+  @Test
+  public void getEntertainment_instantiatesCorrectly_true(){
+    Event testEvent = new Event();
+    assertEquals(true, testEvent.getEntertainment() instanceof String);
+  }
+
 }
